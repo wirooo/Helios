e42793b569149654c102db71caf8ffac246f02fd
wirooo <willh.lu@gmail.com>
2020-01-18 13:33:38 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, render_template, redirect,url_for
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main():
+    return render_template('index.html')
+
+
+app.run()
