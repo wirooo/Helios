f3e77cc64325577a1aa4ebbcec5805aa8d9e220e
wirooo <willh.lu@gmail.com>
2020-01-18 17:42:55 -0500

    create skeleton for web pages

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from flask import Flask, request, jsonify, render_template, redirect,url_for
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
     return render_template('index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    return render_template('search.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
 
 app.run()
