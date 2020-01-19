b67f29b546cc5e8bfadce03d2b272685c60e0fb3
Kevin Lee <es7lee@uwaterloo.ca>
2020-01-18 19:05:26 -0500

    Hello there

--- resume_scraper.py
+++ resume_scraper.py
@@ -1,5 +1,19 @@
 from pdfminer.high_level import *
 
-resume_data = extract_text("resumes/Will Lu Resume.pdf")
+def match_keywords (resume_link):
+    resume_data = extract_text(resume_link)
+    keywords = []
 
-print(resume_data)
+    with open("static/keywords.txt") as keywordsfile:
+     for line in keywordsfile:
+          if line != "":
+             keywords.append(line.rstrip('\n'))
+
+    matched = []
+
+    for kwd in keywords:
+        if kwd in resume_data:
+            matched_kwds.append(kwd)
+
+    print(matched_kwds)
+    return matched_kwds
